2d7a0c8b2fec69d99df38a7a98a28b24ddd8bfd0
bluecb69 <bluecb69@gmail.com>
2024-09-08 22:08:24 +0800

    docs: 修改env App_Name

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { visualizer } from 'rollup-plugin-visualizer';
